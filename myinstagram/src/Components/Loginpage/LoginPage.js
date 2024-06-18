b2351d8088479cb93ab8bf828312ee956f1cfacc
Abdelbaki <itshubie@hotmail.com>
2024-06-18 15:37:30 +0200

    react creation

---
+++ myinstagram/src/Components/Loginpage/LoginPage.js
@@ -0,0 +1,21 @@
+
+import React, { Component } from 'react';
+
+
+class LoginPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { variable: 0 };
+  }
+
+  render() {
+    return (
+      <div>
+      Hello
+      </div>
+    );
+  }
+}
+
+export default  LoginPage;
+
