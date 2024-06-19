b4b4dc009298b3916a3ee82906a6dcdd6955f485
Abdelbaki <itshubie@hotmail.com>
2024-06-19 17:58:48 +0200

    creation loginpage react

--- myinstagram/src/App.js
+++ myinstagram/src/App.js
@@ -1,15 +1,16 @@
+// src/App.js
 
+import React from 'react';
 import './App.css';
-import LoginPage from './components/Loginpage/LoginPage';
-
+import LoginPage from './Components/Loginpage/LoginPage';
 
 function App() {
   return (
     <div className="App">
-      <LoginPage/>
-  
+      <LoginPage />
     </div>
   );
 }
 
 export default App;
+
--- myinstagram/src/Components/Loginpage/LoginPage.js
+++ myinstagram/src/Components/Loginpage/LoginPage.js
@@ -1,21 +1,89 @@
+// src/Components/Loginpage/LoginPage.js
 
-import React, { Component } from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import Grid from '@mui/material/Grid';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import logo from '../../images/logoinsta.png';
+import phoneImage from '../../images/instafon.svg';
+import './LoginPage.css';
+
+const LoginPage = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleUsernameChange = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Username:', username);
+    console.log('Password:', password);
+    // Ajoutez ici votre logique de connexion, par exemple :
+    // - Appel à une API d'authentification
+    // - Validation des informations de connexion
+    // - Redirection vers une nouvelle page
+  };
+
+  return (
+    <Box className="loginPage">
+      <Grid container spacing={2} className="loginContainer">
+        <Grid item xs={12} md={6} className="leftPanel">
+          <img src={logo} alt="Instagram Logo" className="logo" />
+          <Box component="form" onSubmit={handleSubmit} className="loginForm">
+            <TextField
+              fullWidth
+              variant="outlined"
+              margin="normal"
+              label="Username, phone number or email"
+              value={username}
+              onChange={handleUsernameChange}
+              className="textField"
+            />
+            <TextField
+              fullWidth
+              variant="outlined"
+              margin="normal"
+              label="Password"
+              type="password"
+              value={password}
+              onChange={handlePasswordChange}
+              className="textField"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className="loginButton"
+            >
+              Login
+            </Button>
+            <Typography variant="body2" align="center" className="signUpText">
+              <Link to="/signup" className="signUpLink">
+                Don't have an account? Sign up
+              </Link>
+            </Typography>
+          </Box>
+        </Grid>
+        <Grid item xs={12} md={6} className="rightPanel">
+          <img src={phoneImage} alt="Phone Preview" className="phoneImage" />
+        </Grid>
+      </Grid>
+    </Box>
+  );
+};
+
+export default LoginPage;
 
 
-class LoginPage extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { variable: 0 };
-  }
 
-  render() {
-    return (
-      <div>
-      Hello
-      </div>
-    );
-  }
-}
-
-export default  LoginPage;
 
